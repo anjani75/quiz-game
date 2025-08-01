733407ef300e62b3cb9b1ef7bee85a23efafee9e
Anjani Keshri <anjani.keshri@rahitechnologies.com>
2025-08-01 14:07:14 +0530

    Updated class name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from question_model import Question
 from data import question_data
-from quiz_brain import Quizbrain
+from quiz_brain import QuizBrain
 
 num = 1
 question_bank = []
@@ -10,7 +10,7 @@ for ques in question_data:
 #for obj in question_bank:
 #    print(obj.text)
 
-new_quiz = Quizbrain(question_bank)
+new_quiz = QuizBrain(question_bank)
 while new_quiz.still_has_question():
     new_quiz.next_question()
 
